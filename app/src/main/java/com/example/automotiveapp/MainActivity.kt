08b740a5175f92074e924fc13e881f1d0cea771d
Abdul-Rahman Hamza <abdulrahmanhamza2205@gmail.com>
2025-04-24 14:09:57 +0200

    add navigation setup with NavHost and BrandScreen in MainActivity

--- app/src/main/java/com/example/automotiveapp/MainActivity.kt
+++ app/src/main/java/com/example/automotiveapp/MainActivity.kt
@@ -11,37 +11,43 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.automotiveapp.presentations.brand.ui.BrandScreen
 import com.example.automotiveapp.ui.theme.AutomotiveAppTheme
+import com.example.automotiveapp.utils.Routes
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             AutomotiveAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController = rememberNavController()
+
+                Scaffold { paddingValues ->
+                    NavHost(
+                        navController = navController,
+                        startDestination = Routes.BRAND,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(paddingValues)
+                    ) {
+                        composable(Routes.BRAND) {
+                            BrandScreen()
+                        }
+                        composable(Routes.MODELS) {
+                          //  ModelsScreen()
+                        }
+                        composable(Routes.GENERATIONS) {
+                          //  GenerationScreen()
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AutomotiveAppTheme {
-        Greeting("Android")
     }
 }
