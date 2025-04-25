17cdbee262016b3ab194f22be0a5205f4f7d47c9
Abdul-Rahman Hamza <abdulrahmanhamza2205@gmail.com>
2025-04-25 14:53:37 +0300

    add getVehicles method to Repository interface and implementation for vehicle data retrieval

--- app/src/main/java/com/example/automotiveapp/data/repository/RepositoryImpl.kt
+++ app/src/main/java/com/example/automotiveapp/data/repository/RepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.example.automotiveapp.data.repository
 import com.example.automotiveapp.data.remote.ApiService
 import com.example.automotiveapp.data.remote.Brand
 import com.example.automotiveapp.data.remote.Model
+import com.example.automotiveapp.data.remote.Vehicle
 import com.example.automotiveapp.domain.repository.Repository
 import com.example.automotiveapp.utils.Resource
 import kotlinx.coroutines.flow.Flow
@@ -28,16 +29,39 @@ class RepositoryImpl @Inject constructor(
         emit(Resource.Error(e.localizedMessage ?: "An unexpected error occurred"))
     }
 
-    override fun getModels(page: Int, brandId: Int, categoryId: Int): Flow<Resource<List<Model>>> = flow {
-        emit(Resource.Loading())
+    override fun getModels(page: Int, brandId: Int, categoryId: Int): Flow<Resource<List<Model>>> =
+        flow {
+            emit(Resource.Loading())
 
-        val response = apiService.getModels(page, brandId, categoryId)
+            val response = apiService.getModels(page, brandId, categoryId)
+            if (response.isSuccessful && response.body() != null) {
+                emit(Resource.Success(response.body()!!.data))
+            } else {
+                emit(Resource.Error(response.message()))
+            }
+
+        }.catch { e ->
+            emit(Resource.Error(e.localizedMessage ?: "An unexpected error occurred"))
+        }
+
+    override fun getVehicles(
+        identificationAttributeId: Int,
+        modelId: Int,
+        identificationAttributeValueId: Int,
+        category: Int
+    ): Flow<Resource<List<Vehicle>>> = flow {
+        emit(Resource.Loading())
+        val response = apiService.getVehicles(
+            identificationAttributeId,
+            modelId,
+            identificationAttributeValueId,
+            category
+        )
         if (response.isSuccessful && response.body() != null) {
             emit(Resource.Success(response.body()!!.data))
         } else {
             emit(Resource.Error(response.message()))
         }
-
     }.catch { e ->
         emit(Resource.Error(e.localizedMessage ?: "An unexpected error occurred"))
     }
--- app/src/main/java/com/example/automotiveapp/domain/repository/Repository.kt
+++ app/src/main/java/com/example/automotiveapp/domain/repository/Repository.kt
@@ -2,11 +2,17 @@ package com.example.automotiveapp.domain.repository
 
 import com.example.automotiveapp.data.remote.Brand
 import com.example.automotiveapp.data.remote.Model
+import com.example.automotiveapp.data.remote.Vehicle
 import com.example.automotiveapp.utils.Resource
 import kotlinx.coroutines.flow.Flow
 
 interface Repository {
     fun getBrands(categoryId: Int): Flow<Resource<List<Brand>>>
     fun getModels(page: Int, brandId: Int, categoryId: Int): Flow<Resource<List<Model>>>
-
+    fun getVehicles(
+        identificationAttributeId: Int,
+        modelId: Int,
+        identificationAttributeValueId: Int,
+        category: Int
+    ): Flow<Resource<List<Vehicle>>>
 }
